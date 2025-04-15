276fc1f7bb98c0a0799fcde293ef20c25221829c
Floris <t-fvossebeld@microsoft.com>
2025-04-15 15:07:51 +0200

    Exercise 2

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-b3dy7wgfh5ddw FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-b3dy7wgfh5ddw;
+ALTER ROLE db_datawriter ADD MEMBER id-b3dy7wgfh5ddw;
 
